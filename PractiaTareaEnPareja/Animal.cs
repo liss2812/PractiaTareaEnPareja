f784c7a01032803ebfa531e7a7aaa040d319be8a
¨Liss <lissramos2812@mail.com>
2021-09-30 11:28:22 -0500

    Agregar archivos de proyecto.

---
+++ PractiaTareaEnPareja/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PractiaTareaEnPareja
+{
+    class Animal
+    {
+        //propiedades
+        public int animalAge { get; set; }
+
+        public string animalGender { get; set; }
+
+        //metodos
+        public string ismammal()
+        {
+            
+        }
+    }
+}
---
+++ PractiaTareaEnPareja/Fish.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PractiaTareaEnPareja
+{
+    class Fish:Animal
+    {
+
+        public int sizeFt { get; set; }
+
+        //metodos
+        public Boolean canEar { get; set; }
+        public string getInfo()
+        {
+            var Ear = "";
+            if (canEar == true)
+            {
+               Ear = "El animal puede oir";
+            }
+            else
+            {
+                Ear = "El animal no puede oir";
+            }
+            return " El animal tiene  " + animalAge + " años,  y  " + Ear;
+        }
+
+            public string swim()
+            {
+                return "El animal esta nadando ";
+
+
+            }
+        }
+    }
+
+    
+
